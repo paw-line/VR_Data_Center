0f410447abcba264774074e4119c758ebe1d0927
paw-line <paw-line@yandex.ru>
2020-01-17 18:40:10 +0300

    Сделал первый пункт задания про изменение цвета

---
+++ Assets/Scripts/Color_changer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color_changer : MonoBehaviour
+{
+    //Настройки цветов. Текущие - для теплого периода.
+    private float absCold = 11f;
+    private float allowedStart = 21f;
+    private float optimalStart = 23f;
+    private float optimalFinish = 25f;
+    private float allowedFinish = 28f;
+    private float absHeat = 38f;
+
+    public float temp = 23f;
+    private Material material = null; //Устанавливается не префабной связью а в авейке ввиду того что иначе изменяется глобальный материал
+   
+    Color TempToColor(float _temp)
+    {
+        Color newColor = new Color();
+
+        if (_temp <= absCold) //Ниже предела
+        {
+            newColor.r = 0;
+            newColor.g = 0;
+            newColor.b = 1;
+        }
+        else if ((_temp > absCold) && (_temp <= allowedStart)) //Недопустимо холодно.
+        {
+            newColor.r = 0;
+            newColor.g = (_temp - absCold) / (allowedStart - absCold); //Рост
+            newColor.b = 1;
+        }
+        else if ((_temp > allowedStart) && (_temp <= optimalStart)) // допустимо холодно.
+        {
+            newColor.r = 0;
+            newColor.g = 1;
+            newColor.b = (_temp - optimalStart) / (allowedStart - optimalStart); //Падение
+        }
+        else if ((_temp > optimalStart) && (_temp <= optimalFinish)) //Ок температура. Зеленый?
+        {
+            newColor.r = 0;
+            newColor.g = 1;
+            newColor.b = 0;
+        }
+        else if ((_temp > optimalFinish) && (_temp <= allowedFinish)) //Допустимо тепло. Красные
+        {
+            newColor.r = (_temp - optimalFinish) / (allowedFinish - optimalFinish); //Рост
+            newColor.g = 1;
+            newColor.b = 0;
+        }
+        else if ((_temp > allowedFinish) && (_temp <= absHeat)) // Недопустимо жарко. Оранжевый
+        {
+            newColor.r = 1;
+            newColor.g = (_temp - absHeat) / (allowedFinish - absHeat); //Падение
+            newColor.b = 0;
+        }
+        else if (_temp > absHeat)//Выше предела
+        {
+            newColor.r = 1;
+            newColor.g = 0;
+            newColor.b = 0;
+        }
+
+        return (newColor);
+    }
+
+    public void SetTemp(float _temp)
+    {
+        temp = _temp;
+        Color newColor;
+        Color oldColor = material.color;
+        newColor = TempToColor(temp);
+        oldColor.r = newColor.r;
+        oldColor.g = newColor.g;
+        oldColor.b = newColor.b;
+        material.SetColor("_Color", oldColor);
+    }
+
+    public void SetTempCold()
+    {
+        SetTemp(15f);
+    }
+
+    public void SetTempWarm()
+    {
+        SetTemp(30f);
+    }
+
+    public void SetTempOK()
+    {
+        SetTemp(24f);
+    }
+
+    public void SetTemp(float _temp, float _time)
+    {
+        if (_time == 0)
+            SetTemp(_temp);
+    }
+
+    private void Awake()
+    {
+        material = this.GetComponent<MeshRenderer>().material;
+    }
+}
