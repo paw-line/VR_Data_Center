873b971ef8fba3d296ada87ba134a93872228f31
paw-line <paw-line@yandex.ru>
2020-01-20 18:08:51 +0300

    Добавил иконку тревоги на выноску

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[ExecuteInEditMode]
+public class Billboard : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool isRotating3D = false;
+
+    void Update()
+    {
+        if (isRotating3D)
+        {
+            transform.LookAt(Camera.current.transform);
+        }
+        else
+        {
+            Vector3 targetPosition = new Vector3(Camera.current.transform.position.x, transform.position.y, Camera.current.transform.position.z);
+            transform.LookAt(targetPosition);
+        }
+    }
+}
