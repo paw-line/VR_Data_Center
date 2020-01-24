45158e4c003c01b509808eb7f4e0e52d1baf5013
paw-line <paw-line@yandex.ru>
2020-01-24 21:00:19 +0300

    Эксперименты со списком списков для Дистрибутора

---
+++ Assets/Lister.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Lister : MonoBehaviour
+{
+    public PointList Distributor = new PointList();
+}
+ 
+[System.Serializable]
+public class Point
+{
+    public List<GameObject> list;
+}
+
+[System.Serializable]
+public class PointList
+{
+    public List<Point> list;
+}
--- Assets/Scripts/Distributor.cs
+++ Assets/Scripts/Distributor.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class Distributor : MonoBehaviour
 {
     public List<DataSource> sources;
+    //public List<Point> sources;
+    //public List<DataSource> sources;
+
+    public Dictionary<string, DataSource> countries;   
 }
+/*
+[System.Serializable]
+public class Point
+{
+    public List<DataSource> sources;
+}
+*/
